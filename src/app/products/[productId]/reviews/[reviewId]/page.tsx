e39bb930e04215459b5912de444e6404e9f83158
Emerson452 <emerson452pro@gmail.com>
2024-02-07 15:09:07 +0100

    ✨ Not dound page for specifique page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,16 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetails({
   params,
 }: {
   params: { 
     productId: string; 
     reviewId: string;
-};
+}; 
 }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound();
+  }
   return (
     <div>
       <h1>
